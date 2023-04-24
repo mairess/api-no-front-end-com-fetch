cfb7a2390d0913f5ba17f2de1dca5c044f2a21fc
Maires Souza <trabalho.maires@gmail.com>
2023-04-24 11:25:17 -0300

    update main.js

--- fetch-discover-api/main.js
+++ fetch-discover-api/main.js
@@ -7,4 +7,17 @@ function getUsers() {
     .catch(error => console.error(error))
 }
 
+function getUser(){
+    fetch(`${url}/1`)
+    .then(response => response.json())
+    .then (data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
+
 getUsers()
+
+getUser()
